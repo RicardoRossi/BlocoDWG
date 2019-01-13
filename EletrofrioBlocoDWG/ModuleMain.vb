a3dacf16622b65b861ff00b0a7d525835ae8ec8c
Ricardo Rossi <solucoessw@gmail.com>
2019-01-13 00:07:55 -0200

    Add project files.

---
+++ EletrofrioBlocoDWG/ModuleMain.vb
@@ -0,0 +1,15 @@
+﻿Imports System.IO
+Imports SldWorks
+Imports SwConst
+Imports System.Console
+
+Module ModuleMain
+    Dim swApp As SldWorks.SldWorks
+    Dim swModel As ModelDoc2
+
+    Sub Main()
+        swApp = _swApp()
+        swApp.SendMsgToUser("conectado")
+
+    End Sub
+End Module
---
+++ EletrofrioBlocoDWG/SolidWorksSingleton.vb
@@ -0,0 +1,25 @@
+﻿Module SolidWorksSingleton
+    Dim swApp As SldWorks.SldWorks
+    Dim swModel As SldWorks.ModelDoc2
+    Function _swApp() As SldWorks.SldWorks
+        Try
+            If swApp Is Nothing Then
+                swApp = GetObject("", "SldWorks.Application")
+                swApp.Visible = True
+                Return swApp
+            Else
+                Return swApp
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return Nothing
+        End Try
+    End Function
+
+    Function _swModel() As SldWorks.ModelDoc2
+        swModel = _swApp.ActiveDoc
+        Return swModel
+    End Function
+
+End Module
