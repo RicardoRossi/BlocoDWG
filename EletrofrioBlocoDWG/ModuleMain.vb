783169265b40f1e2e2ed77f323b38404e96aa068
Ricardo Rossi <solucoessw@gmail.com>
2019-01-13 19:10:51 -0200

    Classe planilha implementada

--- EletrofrioBlocoDWG/ModuleMain.vb
+++ EletrofrioBlocoDWG/ModuleMain.vb
@@ -2,14 +2,48 @@
 Imports SldWorks
 Imports SwConst
 Imports System.Console
+Imports System.Deployment
 
 Module ModuleMain
     Dim swApp As SldWorks.SldWorks
     Dim swModel As ModelDoc2
+    Dim swCustPropMgr As CustomPropertyManager
+    Dim swExt As ModelDocExtension
+    Dim erro As String
+    Dim aviso As String
 
     Sub Main()
-        swApp = _swApp()
-        swApp.SendMsgToUser("conectado")
+        Dim codigos As List(Of String) = New List(Of String) From {"MONTCPRP0001"}
 
+        swApp = _swApp()
+        'swApp.SendMsgToUser(My.Application.Info.DirectoryPath)
+        Dim fullPathArquivoTemplate = My.Application.Info.DirectoryPath & "\Draw1.SLDDRW"
+        Dim desenho = swApp.OpenDoc6(fullPathArquivoTemplate, swDocumentTypes_e.swDocDRAWING, swOpenDocOptions_e.swOpenDocOptions_ReadOnly, "", erro, aviso)
+
+        Dim tipoErro = CType(erro, swFileLoadError_e).ToString
+        If desenho Is Nothing Then
+            MsgBox(tipoErro)
+            Exit Sub
+        End If
+
+        swModel = swApp.ActiveDoc
+        swExt = swModel.Extension
+        swCustPropMgr = swExt.CustomPropertyManager("")
+
+        For Each codigo In codigos
+            Dim pesoValor As String
+            Dim medidasValor As String
+            Dim nomeArquivoValor = codigo
+
+            swCustPropMgr.Add3("nomeArquivo", swCustomInfoType_e.swCustomInfoText, nomeArquivoValor, swCustomPropertyAddOption_e.swCustomPropertyReplaceValue)
+            swCustPropMgr.Add3("medidas", swCustomInfoType_e.swCustomInfoText, "123x456x789", swCustomPropertyAddOption_e.swCustomPropertyReplaceValue)
+            swCustPropMgr.Add3("peso", swCustomInfoType_e.swCustomInfoText, "11111" & "kg", swCustomPropertyAddOption_e.swCustomPropertyReplaceValue)
+            swModel.SaveAs($"{My.Application.Info.DirectoryPath}\{nomeArquivoValor}.dwg")
+        Next
+
+        Dim lista = PlanilhaExcel.GetDadosColetor()
+        Write(lista)
+        ReadKey()
+        'swModel.EditRebuild3()
     End Sub
 End Module
---
+++ EletrofrioBlocoDWG/PlanilhaExcel.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Runtime.InteropServices
+Imports Excel = Microsoft.Office.Interop.Excel
+Public Class PlanilhaExcel
+
+    Public Shared Function GetDadosColetor() As String
+
+        Dim appXL As Excel.Application = New Excel.Application
+        Dim wbXL As Excel.Workbook = appXL.Workbooks.Open($"{My.Application.Info.DirectoryPath}\nome dos arquivos.xlsx")
+        Dim shXL As Excel.Worksheet = wbXL.Sheets(1)
+        Dim raXL As Excel.Range = shXL.UsedRange
+        shXL = wbXL.ActiveSheet
+
+        Dim rowCount = raXL.Rows.Count
+        Dim colCount = raXL.Columns.Count
+        Dim coluna_A = ""
+        Dim nomeArquivos = ""
+        Try
+            For i = 2 To rowCount
+                Dim cellValue = CType(raXL.Cells(i, 1), Excel.Range) 'Cells retorna oject que e convertido para Range
+                coluna_A = cellValue.Value.ToString
+                nomeArquivos += coluna_A & Environment.NewLine
+            Next
+        Finally
+            GC.Collect()
+            GC.WaitForPendingFinalizers()
+            Marshal.ReleaseComObject(raXL)
+            Marshal.ReleaseComObject(shXL)
+            wbXL.Close()
+            Marshal.ReleaseComObject(wbXL)
+            appXL.Quit()
+            Marshal.ReleaseComObject(appXL)
+        End Try
+        Return nomeArquivos
+    End Function
+End Class
